8a2a0c93720b522db0b20ecda12a75f7b739c4f4
gonzalo zoloaga <gonzalozoloaga44@gmail.com>
2022-09-23 16:11:12 -0300

    Repositories, observation and measurement

---
+++ src/main/java/com/certant/vtv/repository/MeasurementRepository.java
@@ -0,0 +1,9 @@
+package com.certant.vtv.repository;
+
+import com.certant.vtv.model.Measurement;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MeasurementRepository extends JpaRepository<Measurement, Long> {
+}
---
+++ src/main/java/com/certant/vtv/repository/ObservationRepository.java
@@ -0,0 +1,9 @@
+package com.certant.vtv.repository;
+
+import com.certant.vtv.model.Observation;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ObservationRepository extends JpaRepository<Observation, Long> {
+}
