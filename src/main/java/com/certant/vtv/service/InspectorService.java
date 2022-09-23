4834be976e834f1697910bfaf3f0a5225484f4d2
gonzalo zoloaga <gonzalozoloaga44@gmail.com>
2022-09-23 16:11:53 -0300

    INspector service

--- src/main/java/com/certant/vtv/service/InspectorService.java
+++ src/main/java/com/certant/vtv/service/InspectorService.java
@@ -1,11 +1,10 @@
 package com.certant.vtv.service;
 
-import com.certant.vtv.model.Costumer;
 import com.certant.vtv.model.Inspector;
 
 import java.util.List;
 
-public interface InstructorService {
+public interface InspectorService {
     Inspector createInspector(Inspector inspector);
     Inspector getInspector(Long id);
     List<Inspector> getAll();
---
+++ src/main/java/com/certant/vtv/service/impl/InspectorServiceImpl.java
@@ -0,0 +1,46 @@
+package com.certant.vtv.service.impl;
+
+import com.certant.vtv.model.Inspector;
+import com.certant.vtv.repository.InspectorRepository;
+import com.certant.vtv.service.InspectorService;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@AllArgsConstructor
+public class InspectorServiceImpl implements InspectorService {
+
+    private InspectorRepository inspectorRepository;
+    @Override
+    public Inspector createInspector(Inspector inspector) {
+
+        return inspectorRepository.save(inspector);
+    }
+
+    @Override
+    public Inspector getInspector(Long id) {
+
+        Optional<Inspector> inspector = inspectorRepository.findById(id);
+
+        return inspector.orElse(null);
+    }
+
+    @Override
+    public List<Inspector> getAll() {
+
+        return inspectorRepository.findAll();
+    }
+
+    @Override
+    public Inspector updateInspector(Long id, Inspector inspector) {
+        return null;
+    }
+
+    @Override
+    public void deleteInspector(Long id) {
+        inspectorRepository.deleteById(id);
+    }
+}
