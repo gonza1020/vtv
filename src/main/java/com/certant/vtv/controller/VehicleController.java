37b4b1a8f96e5b5ed662fea69b3a6e6d2202233c
gonzalo zoloaga <gonzalozoloaga44@gmail.com>
2022-09-26 10:17:02 -0300

    swagger annotations

--- src/main/java/com/certant/vtv/controller/VehicleController.java
+++ src/main/java/com/certant/vtv/controller/VehicleController.java
@@ -1,9 +1,9 @@
 package com.certant.vtv.controller;
 
-import com.certant.vtv.model.Costumer;
+import com.certant.vtv.dto.VehicleDto;
 import com.certant.vtv.model.Vehicle;
-import com.certant.vtv.service.CostumerService;
-import com.certant.vtv.service.VehicleService;
+import com.certant.vtv.service.impl.VehicleServiceImpl;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -16,30 +16,44 @@ import java.util.List;
 @AllArgsConstructor
 public class VehicleController {
 
-    private VehicleService vehicleService;
+    private VehicleServiceImpl vehicleService;
 
 
+    @Operation(summary = "create vehicle")
     @PostMapping()
-    ResponseEntity<Vehicle> createCostumer(@RequestBody Vehicle vehicle){
+    ResponseEntity<Vehicle> createVehicle(@RequestBody Vehicle vehicle){
         return new ResponseEntity<>(vehicleService.createVehicle(vehicle), HttpStatus.CREATED);
     }
 
+    @Operation(summary = "update vehicle")
+
     @PutMapping("/{id}")
-    ResponseEntity<Vehicle> updateCostumer( @PathVariable  Long id,@RequestBody  Vehicle vehicle){
+    ResponseEntity<Vehicle> updateVehicle( @PathVariable  Long id,@RequestBody  Vehicle vehicle){
         return new ResponseEntity<>(vehicleService.updateVehicle(id,vehicle), HttpStatus.ACCEPTED);
     }
 
+
+    @Operation(summary = "get vehicle")
     @GetMapping("/{id}")
-    ResponseEntity<Vehicle> getCostumer( @PathVariable  Long id){
-        return new ResponseEntity<>(vehicleService.getVehicle(id), HttpStatus.ACCEPTED);
+    ResponseEntity<VehicleDto> getVehicle( @PathVariable  Long id){
+        return new ResponseEntity<>(vehicleService.getVehicle(id), HttpStatus.OK);
     }
+
+    @Operation(summary = "get all vehicles")
 
     @GetMapping()
-    ResponseEntity<List<Vehicle>> getAll(){
-        return new ResponseEntity<>(vehicleService.getAll(), HttpStatus.ACCEPTED);
+    ResponseEntity<List<VehicleDto>> getAll(){
+        return new ResponseEntity<>(vehicleService.getAll(), HttpStatus.OK);
     }
 
-    @DeleteMapping()
+    @Operation(summary = "get vehicle by its condition")
+    @GetMapping("/condition")
+    ResponseEntity<List<VehicleDto>> getVehiclesByConditions(@RequestParam String condition){
+        return new ResponseEntity<>(vehicleService.getVehiclesCondition(condition),HttpStatus.OK);
+    }
+
+    @Operation(summary = "delete vehicle")
+    @DeleteMapping("{id}")
     ResponseEntity<?> deleteCostumer(@PathVariable Long id){
         vehicleService.deleteVehicle(id);
         return ResponseEntity.noContent().build();
