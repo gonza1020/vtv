bb3f5b5be417037b83458d017008efd9dbb2952b
gonzalo zoloaga <gonzalozoloaga44@gmail.com>
2022-09-26 10:14:13 -0300

    findByname method in costumer

--- src/main/java/com/certant/vtv/repository/CostumerRepository.java
+++ src/main/java/com/certant/vtv/repository/CostumerRepository.java
@@ -6,4 +6,6 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface CostumerRepository extends JpaRepository<Costumer,Long> {
+    Costumer findByName(String name);
+
 }
