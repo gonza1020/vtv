502c1f915c192ff4cd69abc2d8bedc9b88c85173
gonzalo zoloaga <gonzalozoloaga44@gmail.com>
2022-10-20 09:45:25 -0300

    Vehicle data validation

--- src/main/java/com/certant/vtv/model/Vehicle.java
+++ src/main/java/com/certant/vtv/model/Vehicle.java
@@ -8,6 +8,8 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
 import java.util.List;
 
 @Entity
@@ -20,8 +22,11 @@ public class Vehicle {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Pattern(regexp = "[A-Za-z]{3}[\\d]{3}|[A-Za-z]{2}[\\d]{3}[A-Za-z]{2}", message = "Patente incorrecta. Los formatos validos son: ABC123 o AB123CD")
     private String licensePlate;
+    @NotBlank(message = "La marca no puede estar vacia")
     private String brand;
+    @NotBlank(message = "El modelo no puede estar vacio")
     private String model;
     @ManyToOne(targetEntity = Costumer.class)
     @JoinColumn(name = "costumer_id")
