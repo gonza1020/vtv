721f310331e61024ee19f8821e3f687811b0207e
gonzalo zoloaga <gonzalozoloaga44@gmail.com>
2022-10-20 14:54:19 -0300

    Repository of car and cyce added

---
+++ src/main/java/com/certant/vtv/repository/CarRepository.java
@@ -0,0 +1,9 @@
+package com.certant.vtv.repository;
+
+import com.certant.vtv.model.Car;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CarRepository extends JpaRepository<Car,Long> {
+}
